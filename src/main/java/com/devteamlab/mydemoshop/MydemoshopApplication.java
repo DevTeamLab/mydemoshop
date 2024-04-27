e08fa20652944928fdc5c42d8880447fdf36a0a6
mkhavr <mkhavr.personal@gmail.com>
2024-04-28 00:13:21 +0300

    init commit

---
+++ src/main/java/com/devteamlab/mydemoshop/MydemoshopApplication.java
@@ -0,0 +1,13 @@
+package com.devteamlab.mydemoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydemoshopApplication.class, args);
+	}
+
+}
